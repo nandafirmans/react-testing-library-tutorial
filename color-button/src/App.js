68046870c43d06545232e091709a13b4863a1416
Nanda Firmansyah <nandafirmans@gmail.com>
2022-06-30 12:54:32 +0700

    implement button click change color

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isButtonRed, setIsButtonRed] = useState(true);
+  const backgroundColor = isButtonRed ? "red" : "blue";
+  const buttonLabel = isButtonRed ? "Change to blue" : "Change to red";
+
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button
+        style={{ backgroundColor }}
+        onClick={() => setIsButtonRed(!isButtonRed)}
+      >
+        {buttonLabel}
+      </button>
     </div>
   );
 }
